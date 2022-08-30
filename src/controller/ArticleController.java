2d370b976cb9300a772b318b410f36626b12e3a6
gpfla3690 <gpfla3690@gmail.com>
2022-08-30 17:02:38 +0900

    게시글 수정 기능 구현

--- src/controller/ArticleController.java
+++ src/controller/ArticleController.java
@@ -6,6 +6,8 @@ import infra.Request;
 import service.ArticleService;
 import utils.Util;
 
+import java.io.PrintStream;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class ArticleController implements Controller{
@@ -30,6 +32,9 @@ public class ArticleController implements Controller{
                 break;
             case "delete":
                 delete(request);
+                break;
+            case "modify":
+                modify(request);
                 break;
             default:
                 System.out.println("존재하지 않는 요청입니다.");
@@ -85,6 +90,7 @@ public class ArticleController implements Controller{
         System.out.println("제목 : " + findArticle.getTitle());
         System.out.println("내용 : " + findArticle.getBody());
         System.out.println("작성일 : " + findArticle.getRegDate());
+        System.out.println("수정일 : " + findArticle.getUpdateDate());
 
     }
 
@@ -115,6 +121,44 @@ public class ArticleController implements Controller{
         articleService.delete(findArticle);
 
         System.out.println("성공적으로 삭제되었습니다.");
+
+    }
+
+    public void modify(Request request){
+
+        String paramKey = "id";
+
+        if(!Util.hasParam(request, paramKey)){
+            System.out.println(paramKey + "파라미터가 필요합니다.");
+            return;
+        }
+
+        int articleId = request.getParameterIntValue(paramKey);
+
+        Article findArticle = articleService.getById(articleId);
+
+        if(findArticle == null){
+            System.out.println("해당 게시글은 존재하지 않습니다.");
+            return;
+        }
+
+        if(!request.getLogonMemberId().equals(findArticle.getAuthor())){
+            System.out.println("권한이 없습니다.");
+            return;
+        }
+
+        System.out.println(" == " + articleId + "번 게시글 수정 ==");
+        System.out.print("제목 : ");
+        String newTitle = sc.nextLine().trim();
+
+        System.out.print("내용 : ");
+        String newBody = sc.nextLine().trim();
+
+        findArticle.setTitle(newTitle);
+        findArticle.setBody(newBody);
+        findArticle.setUpdateDate(LocalDateTime.now());
+
+        System.out.println("게시글이 성공적으로 수정되었습니다.");
 
     }
 }
